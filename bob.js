b7e2d2ea7ffe6e691148c5201b4c28d5d848e0eb
GabiG <gabrielg@wix.com>
2017-09-16 14:57:52 +0300

    b

--- bob.js
+++ bob.js
@@ -3,6 +3,6 @@ const s = require('shelljs');
 console.log('RUNNING')
 console.log(22, process.env.GIT_PARAMS);
 
-const files = s.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').stdout.split(',');
+const files = s.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD').stdout.split('\n');
 
 console.log(files);
