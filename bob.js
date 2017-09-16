a135f2145097b6485672ac0dc5a3abd5c5e035de
GabiG <gabrielg@wix.com>
2017-09-16 14:56:37 +0300

    b

--- bob.js
+++ bob.js
@@ -1,3 +1,8 @@
+
+const s = require('shelljs');
 console.log('RUNNING')
 console.log(22, process.env.GIT_PARAMS);
 
+const files = s.exec('git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD');
+
+console.log(files);
