7266ce0926428dfc49328a7672ce6a9e19e713f6
GabiG <gabrielg@wix.com>
2017-09-18 23:52:13 +0300

    Dfg

--- bob.js
+++ bob.js
@@ -1,14 +1,14 @@
 
-const s = require('shelljs');
-console.log('RUNNING')
-const params =  (process.env.GIT_PARAMS || '').split(' ');
+// const s = require('shelljs');
+// console.log('RUNNING')
+// const params =  (process.env.GIT_PARAMS || '').split(' ');
 
-const ohead = params[0];
-const nhead = params[1];
+// const ohead = params[0];
+// const nhead = params[1];
 
-if (ohead && nhead) {
-	const files = s.exec(`git diff-tree -r --name-only --no-commit-id ${ohead} ${nhead}`).stdout.split('\n').map((f) => `--files-changed ${f}`);
-	s.exec('merlin build-changed --cwd root ' + files.join(' '), {shell: '/bin/zsh'});
-} else {
-    console.log('no ohead head');
-}
+// if (ohead && nhead) {
+// 	const files = s.exec(`git diff-tree -r --name-only --no-commit-id ${ohead} ${nhead}`).stdout.split('\n').map((f) => `--files-changed ${f}`);
+// 	s.exec('merlin build-changed --cwd root ' + files.join(' '), {shell: '/bin/zsh'});
+// } else {
+//     console.log('no ohead head');
+// }
